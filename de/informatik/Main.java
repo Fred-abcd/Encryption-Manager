ba442ebd553e3ce2a25220583fa7b128e1f398c8
requiem139 <alpayuerkuendag@gmail.com>
2024-03-04 10:19:30 +0100

    c

--- de/informatik/Main.java
+++ de/informatik/Main.java
@@ -1,5 +1,11 @@
 package de.informatik;
 
+import de.informatik.utils.Logger;
+
 public class Main {
-    //TODO
+    public static Logger logger = new Logger();
+    public static void main(String[] args) {
+        logger.logInfo("Please choose one of the following options: ");
+
+    }
 }
