5e04018dc9486937236db717a5d93bb37028d735
requiem139 <alpayuerkuendag@gmail.com>
2024-03-04 10:29:01 +0100

    c

--- de/informatik/Main.java
+++ de/informatik/Main.java
@@ -1,11 +1,20 @@
 package de.informatik;
 
+import de.informatik.encryptions.EncryptionManager;
 import de.informatik.utils.Logger;
+
+import java.util.Scanner;
 
 public class Main {
     public static Logger logger = new Logger();
+    public static EncryptionManager manager = new EncryptionManager();
     public static void main(String[] args) {
-        logger.logInfo("Please choose one of the following options: ");
+        logger.logInfo("Loading all Encryptions...");
+        manager.init();
+        logger.logSuccess("Loaded!");
 
+
+        logger.logInfo("Please choose one of the following options: ");
+        manager.input(new Scanner(System.in));
     }
 }
