5bd262601a3a2513a9af7df02f360f803cd6576e
Fred <@>
2024-03-04 10:02:20 +0100

    todos usw

---
+++ de/informatik/encryptions/Encryption.java
@@ -0,0 +1,32 @@
+package de.informatik.encryptions;
+
+public abstract class Encryption {
+
+    public final String name;
+    public String plaintext;
+    public String key;
+
+
+    public Encryption(String name) {
+        this.name = name;
+    }
+
+    public Encryption(String name, String key) {
+        this.name = name;
+        this.key = key;
+    }
+
+    abstract public void onExecute(String plaintext);
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPlaintext() {
+        return plaintext;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
---
+++ de/informatik/encryptions/EncryptionManager.java
@@ -0,0 +1,7 @@
+package de.informatik.encryptions;
+
+import java.util.ArrayList;
+
+public class EncryptionManager {
+    //TODO
+}
