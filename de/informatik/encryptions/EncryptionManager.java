45df253f5d2fcd44cc00154f1a533247dabe29d7
Fred <@>
2024-03-04 10:21:44 +0100

    edits

--- de/informatik/encryptions/EncryptionManager.java
+++ de/informatik/encryptions/EncryptionManager.java
@@ -1,7 +1,36 @@
 package de.informatik.encryptions;
 
+import de.informatik.Main;
+import de.informatik.utils.InputHelper;
+import de.informatik.utils.Logger;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class EncryptionManager {
-    //TODO
+    public ArrayList<Encryption> encryptions = new ArrayList<>();
+    public List<String> encryptionNames = new ArrayList<>();
+
+    public void init() {
+
+        for (Encryption encryption : encryptions) {
+            encryptionNames.add(encryption.getName());
+            encryption.onLoad();
+        }
+    }
+
+    public void input(String input) {
+        String[] args = input.split("\\s+");
+        for (Encryption encryption : encryptions) {
+            if (encryption.getName().equalsIgnoreCase(input)) {
+                //noch falsch
+                encryption.onExecute(args[1]);
+                return;
+            }
+        }
+        String mostSimilarString = InputHelper.findMostSimilarString(input, encryptionNames);
+        Main.logger.logError("Module " + input + " not found! Did you mean " + Logger.ANSI_YELLOW + mostSimilarString + Logger.ANSI_RED + "?");
+        return;
+    }
+
 }
